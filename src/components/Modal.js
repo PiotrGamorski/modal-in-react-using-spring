264eca01fcc9d5dba33738b13257ba42e6bdd2af
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2020-12-09 14:18:30 +0100

    Modal creared

---
+++ src/components/Modal.js
@@ -0,0 +1,88 @@
+import React, {useRef, useEffect, useCallback} from "react";
+import {useSpring, animated} from "react-spring";
+import styled from "styled-components";
+import { MdClose } from "react-icons/md";
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  width: 600px;
+  height: 400px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+
+const ModalImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background: #000;
+`;
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32;
+  padding: 0;
+  z-index: 10;
+`;
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <>
+      {showModal ? (
+        <Background>
+          <ModalWrapper showModal={showModal}>
+            <ModalImg src={process.env.PUBLIC_URL + "./Assets/modal.jpg"} alt="camera" />
+            <ModalContent>
+              <h1>Put some content in here</h1>
+              <p>Put some text in here as well</p>
+              <button>Press the button</button>
+            </ModalContent>
+            <CloseModalButton
+              aria-label="Close modal"
+              onClick={() => setShowModal((prev) => !prev)}
+            />
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
